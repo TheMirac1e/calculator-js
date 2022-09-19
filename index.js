c2435d0d4761f5f0c32167121e387c3662d6e938
Even Koshliak <even.koshliak.fr@gmail.com>
2022-09-19 23:55:23 +0300

    add js class, and clear method

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hello!')
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = null;
+    }
+
+    appendNumber(number) {
+        this.currentOperand += number.toString();
+        console.log(this.currentOperand);
+    }
+
+    updateDisplay() {}
+}
+
+const buttonNumber = document.querySelectorAll('[data-number]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+buttonNumber.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerHTML);
+    });
+});
