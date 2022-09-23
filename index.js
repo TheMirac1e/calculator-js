14313c3fe26347f55c2b7b01f98282a8954c688d
Even Koshliak <even.koshliak.fr@gmail.com>
2022-09-23 17:26:12 +0300

    fix dot, add some styles

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-        if(number === '.' || this.currentOperand.includes('.')) return;
+        if(number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand += number.toString();
     }
 
